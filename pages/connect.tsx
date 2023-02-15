83b67476c10568ad1516940b37872f0b60088868
Atharva <adrooney322@gmail.com>
2023-02-15 16:23:55 +0000

    jsonwebtoken fixed

--- pages/connect.tsx
+++ pages/connect.tsx
@@ -1,28 +1,27 @@
-import React, { FC, useState } from 'react';
-import { useRouter } from 'next/router'
-import jwt from 'jsonwebtoken';
-// ! inside the React Component 
+import React, { FC, useState } from "react";
+import { useRouter } from "next/router";
+import jwt from "jsonwebtoken";
+// ! inside the React Component
 
-
-const Connect: FC= () => {
- 
+const Connect: FC = () => {
   const [decoded, setDecoded] = useState("hi");
-    const router = useRouter()
-    const token = router.query.token as string;
-    const JWT_KEY = 'KEYY' as string;
+  const router = useRouter();
+  const token = router.query.token as string;
+  const JWT_KEY = "KEY" as string;
 
-    // Verify and decode the JWT token
-try {
-  const decodedt = jwt.verify(token, JWT_KEY);
-  setDecoded(decodedt as string)
-  console.log(decodedt);
-} catch (error) {
-  console.error(error);
-}
+  // Verify and decode the JWT token
+  try {
+    console.log("token", token);
+    console.log("key", JWT_KEY);
+    const decodedt = jwt.verify(token, JWT_KEY);
+    console.log("decoded", decodedt);
+    setDecoded(decodedt as string);
+    console.log(decodedt);
+  } catch (error) {
+    console.error(error);
+  }
 
-  return <div>
-    {decoded}
-    </div>;
+  return <div>{decoded}</div>;
 };
 
 export default Connect;
